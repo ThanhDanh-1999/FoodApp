9ce9aeb7a948e036a2970e5e57d28f6653ae8410
DangHuuTai <taidang290999@gmail.com>
2021-04-14 12:45:14 +0700

    add data binding feature

--- app/src/main/java/com/example/foodapp/LogInActivity.kt
+++ app/src/main/java/com/example/foodapp/LogInActivity.kt
@@ -2,10 +2,20 @@ package com.example.foodapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.foodapp.databinding.ActivityLogInBinding
+import kotlinx.android.synthetic.main.activity_log_in.*
 
 class LogInActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityLogInBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_log_in)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_log_in)
+
+        back_from_login.setOnClickListener {
+            finish()
+        }
     }
 }
